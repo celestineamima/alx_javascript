00e9aa629821667abf9984993219c009f06d3384
celestineamima <celestineamima@gmail.com>
2023-09-14 21:18:56 +0300

    my javascript files

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,5 @@
+#!/bin/usr/node
+exports.add = function (num1, num2) {
+    let result = num1 + num2;
+    return result;
+};
---
+++ javascript-warm_up/README.md
@@ -0,0 +1,5 @@
+# First constant, first print
+# 3 languages
+# Loop to languages
+# Object
+# Add file
